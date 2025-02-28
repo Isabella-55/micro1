bedd325844b4cb769ce3ef9f67046c77e3453f9c
Ashly1250 <ashlymunoz@unicauca.edu.co>
2025-02-28 14:53:55 -0500

    Update ledreta.asm

--- ledreta.asm
+++ ledreta.asm
@@ -1,6 +1,4 @@
-
 #include "p18f4550.inc"
-
   CONFIG  FOSC = INTOSC_EC      ; Oscillator Selection bits (Internal oscillator, CLKO function on RA6, EC used by USB (INTCKO))
   CONFIG  WDT = OFF             ; Watchdog Timer Enable bit (WDT disabled (control is placed on the SWDTEN bit))
   CONFIG  LVP = OFF             ; Single-Supply ICSP Enable bit (Single-Supply ICSP disabled)
@@ -9,16 +7,16 @@
      LIST P=18F4550           ; Especifica el microcontrolador PIC18F4550
    
    ;Se definen variables en la RAM
-    UDATA ;indica el inicio de una secci髇 de datos no inicializados en la RAM.
+    UDATA ;indica el inicio de una secci贸n de datos no inicializados en la RAM.
 CONTADOR1   RES 1      ; Reserva 1 byte en RAM para la variable CONTADOR1
 CONTADOR2   RES 1      ; Reserva 1 byte en RAM para la variable CONTADOR2
 CONTADOR3   RES 1      ; Reserva 1 byte en RAM para la variable CONTADOR3
 
     
-    ORG 0x0000          ; Direcci髇 de inicio en memoria
+    ORG 0x0000          ; Direcci贸n de inicio en memoria
     GOTO Inicio         ; Salta a  Inicio cuando prende micro
 
-    ORG 0x0020          ; Direcci髇 donde comienza el c骴igo principal
+    ORG 0x0020          ; Direcci贸n donde comienza el c贸digo principal
 
 Inicio:
    
@@ -47,12 +45,12 @@ RETARDO_5S_LOOP2:
     MOVLW D'200'        ; Carga el valor decimal 200 en WREG
     MOVWF CONTADOR3     ; Guarda el valor en CONTADOR3 
 RETARDO_5S_LOOP3:
-    NOP                 ; No hace nada permite que se generar un peque駉 retardo
+    NOP                 ; No hace nada permite que se generar un peque帽o retardo
     NOP                 ; No hace nada
     NOP                 ; No hace nada
-    DECFSZ CONTADOR3, 1 ; Decrementa CONTADOR3 y si llega a 0, salta a la siguiente instrucci髇
+    DECFSZ CONTADOR3, 1 ; Decrementa CONTADOR3 y si llega a 0, salta a la siguiente instrucci贸n
     GOTO RETARDO_5S_LOOP3  ; Si CONTADOR3 no es 0, sigue repitiendo el bucle interno
-    DECFSZ CONTADOR2, 1 ; Decrementa CONTADOR2 y si llega a 0, salta a la siguiente instrucci髇
+    DECFSZ CONTADOR2, 1 ; Decrementa CONTADOR2 y si llega a 0, salta a la siguiente instrucci贸n
     GOTO RETARDO_5S_LOOP2  ; Si CONTADOR2 no es 0, sigue repitiendo el bucle medio
     DECFSZ CONTADOR1, 1 ; Decrementa CONTADOR1 y si llega a 0, sale de la subrutina
     GOTO RETARDO_5S_LOOP1  ; Si CONTADOR1 no es 0, sigue repitiendo el bucle externo
@@ -69,16 +67,14 @@ RETARDO_2S_LOOP2:
     MOVLW D'200'        ; Carga el valor decimal 200 en WREG
     MOVWF CONTADOR3     ; Guarda el valor en CONTADOR3 (bucle interno)
 RETARDO_2S_LOOP3:
-    NOP                 ; No hace nada (permite generar un peque駉 retardo)
+    NOP                 ; No hace nada (permite generar un peque帽o retardo)
     NOP                 ; No hace nada
     NOP                 ; No hace nada
-    DECFSZ CONTADOR3, 1 ; Decrementa CONTADOR3 y si llega a 0, salta a la siguiente instrucci髇
+    DECFSZ CONTADOR3, 1 ; Decrementa CONTADOR3 y si llega a 0, salta a la siguiente instrucci贸n
     GOTO RETARDO_2S_LOOP3  ; Si CONTADOR3 no es 0, sigue repitiendo el bucle interno
-    DECFSZ CONTADOR2, 1 ; Decrementa CONTADOR2 y si llega a 0, salta a la siguiente instrucci髇
+    DECFSZ CONTADOR2, 1 ; Decrementa CONTADOR2 y si llega a 0, salta a la siguiente instrucci贸n
     GOTO RETARDO_2S_LOOP2  ; Si CONTADOR2 no es 0, sigue repitiendo el bucle medio
     DECFSZ CONTADOR1, 1 ; Decrementa CONTADOR1 y si llega a 0, sale de la subrutina
     GOTO RETARDO_2S_LOOP1  ; Si CONTADOR1 no es 0, sigue repitiendo el bucle externo
     RETURN             ; Regresa al punto donde fue llamada la subrutina
-
-   
     END               
